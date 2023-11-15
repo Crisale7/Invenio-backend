3a7a1f8a3ccf90db0db91f8a0ab72e0fbd3ccd7b
Crisale7 <cristhian.yanez@ucb.edu.bo>
2023-11-15 00:29:55 -0400

    Primer commit

---
+++ src/test/java/com/invenio/invenio/InvenioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invenio.invenio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
