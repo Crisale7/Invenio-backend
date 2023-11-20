dbb5be37816ce6f952f7f59486b885c1ef8e3618
Crisale7 <cristhian.yanez@ucb.edu.bo>
2023-11-20 05:12:33 -0400

    Modelo funcional

---
+++ src/main/java/com/invenio/invenio/api/Modeloapi.java
@@ -0,0 +1,31 @@
+package com.invenio.invenio.api;
+
+import com.invenio.invenio.bl.Modelobl;
+import com.invenio.invenio.dto.Modelodto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/modelo")
+public class Modeloapi {
+
+    private final Modelobl modelobl;
+    private static final Logger LOG = LoggerFactory.getLogger(Modeloapi.class);
+
+    @Autowired
+    public Modeloapi(Modelobl modelobl) {
+        this.modelobl = modelobl;
+    }
+
+    @PostMapping("/crear")
+    public int CrearModelo(@RequestParam String modelo, @RequestParam int Tipo_Producto_tipo_producto_id) {
+        LOG.info("Recibida solicitud POST para crear modelo con nombre: {}", modelo);
+        Modelodto modelodto = modelobl.CrearModelo(modelo, Tipo_Producto_tipo_producto_id);
+        return modelodto.getModelo_id();
+    }
+}
---
+++ src/main/java/com/invenio/invenio/bl/Modelobl.java
@@ -0,0 +1,29 @@
+package com.invenio.invenio.bl;
+
+import com.invenio.invenio.dao.Modelo;
+import com.invenio.invenio.dao.Repository.Modelorepository;
+import com.invenio.invenio.dto.Modelodto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Modelobl {
+    private final Modelorepository modelorepository;
+    private static final Logger LOG = LoggerFactory.getLogger(Modelobl.class);
+
+    @Autowired
+    public Modelobl(Modelorepository modelorepository) {
+        this.modelorepository = modelorepository;
+    }
+
+    public Modelodto CrearModelo(String modelo, int Tipo_Producto_tipo_producto_id){
+        LOG.info("Creando modelo con nombre: {}", modelo);
+        Modelo modeloEntity = new Modelo();
+        modeloEntity.setModelo(modelo);
+        modeloEntity.setTipo_Producto_tipo_producto_id(Tipo_Producto_tipo_producto_id);
+        modelorepository.save(modeloEntity);
+        return new Modelodto(modeloEntity.getModelo_id(), Tipo_Producto_tipo_producto_id, modelo);
+    }
+}
