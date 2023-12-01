2ebca6693e36de72892068ca4c00b982ac50f741
Crisale7 <cristhian.yanez@ucb.edu.bo>
2023-12-01 01:47:13 -0400

    Mejoras de backend

--- src/main/java/com/invenio/invenio/api/Usuarioapi.java
+++ src/main/java/com/invenio/invenio/api/Usuarioapi.java
@@ -5,10 +5,7 @@ import com.invenio.invenio.dto.Usuariodto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/usuario")
@@ -22,10 +19,8 @@ public class Usuarioapi {
         this.usuariobl = usuariobl;
     }
 
-    @PostMapping ("/crear")
-    public int CrearUsuario(@RequestParam String hash_contrasena, @RequestParam String nombre, @RequestParam String usuario, @RequestParam int Asignacion_asignacion_id) {
-        LOG.info("Recibida solicitud POST para crear usuario con nombre: {}", nombre);
-        Usuariodto usuariodto = usuariobl.CrearUsuario(hash_contrasena, nombre, usuario, Asignacion_asignacion_id);
-        return usuariodto.getUsuario_id();
+    @PostMapping("/crear")
+    public Usuariodto crearUsuario(@RequestParam String contrasena, @RequestParam String nombre, @RequestParam String usuario, @RequestParam int Asignacion_asignacion_id) {
+        return usuariobl.CrearUsuario(contrasena, nombre, usuario, Asignacion_asignacion_id);
     }
 }
--- src/main/java/com/invenio/invenio/bl/Usuariobl.java
+++ src/main/java/com/invenio/invenio/bl/Usuariobl.java
@@ -8,6 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 @Service
 public class Usuariobl {
     private final Usuariorepository usuariorepository;
@@ -18,15 +23,30 @@ public class Usuariobl {
         this.usuariorepository = usuariorepository;
     }
 
-    public Usuariodto CrearUsuario(String hash_contrasena, String nombre, String usuario, int Asignacion_asignacion_id){
+    public Usuariodto CrearUsuario(String contrasena, String nombre, String usuario, int Asignacion_asignacion_id){
         LOG.info("Creando usuario con nombre: {}", nombre);
         Usuario usuarioEntity = new Usuario();
-        usuarioEntity.setHash_contrasena(hash_contrasena);
+        usuarioEntity.setHash_contrasena(getSHA256(contrasena));
         usuarioEntity.setNombre(nombre);
         usuarioEntity.setUsuario(usuario);
         usuarioEntity.setAsignacion_asignacion_id(Asignacion_asignacion_id);
         usuariorepository.save(usuarioEntity);
         return new Usuariodto(usuarioEntity.getHash_contrasena(), usuarioEntity.getNombre(), usuarioEntity.getUsuario(), usuarioEntity.getAsignacion_asignacion_id());
+    }
+
+    private static String getSHA256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
